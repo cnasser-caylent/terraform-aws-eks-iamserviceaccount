419a9c98fa526da4ef384fe38ee98bbbe2c147f0
Carlos Nasser <126293002+cnasser-caylent@users.noreply.github.com>
2023-03-16 16:50:13 -0600

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -24,7 +24,6 @@ resource "aws_iam_role" "role_service_account" {
                 "Action": "sts:AssumeRoleWithWebIdentity",
                 "Condition": {
                     "StringEquals": {
-                        "${var.oidc_provider}:aud": "sts.amazonaws.com",
                         "${var.oidc_provider}:sub": "system:serviceaccount:${var.service_account_namespace}:${var.service_account_name}"
                     }
                 }
